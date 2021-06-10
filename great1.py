2452ab32c0ded77cf9465319649c86287fc0b631
Arman Zaidi <zaidiarman90@gmail.com>
2021-06-10 09:57:47 +0530

    initial commit

---
+++ great1.py
@@ -0,0 +1,24 @@
+num1 = int(input("enter number 1: "))
+num2 = int(input("enter number 2: "))
+num3 = int(input("enter number 3: "))
+num4 = int(input("enter number 4: "))
+
+mylist = [num1 , num2 , num3 , num4]
+mylist.sort()
+print(mylist[-1])
+
+
+# if (num1>num4):
+# 	f1 = num1
+# else:
+# 	f1 = num4
+
+# if (num2>num3):
+# 	f2 = num2
+# else:
+# 	f2 = num3
+
+# if(f2>f1):
+# 	print(str(f2) + "is greatest")
+# else:
+# 	print(str(f1) + "is greatest")
